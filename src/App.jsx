27b8dec2eac607c9b7337e73b4a23245dd8d0d7f
oliveiraDevops <oliveira.devops@gmail.com>
2022-03-17 20:05:33 -0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Initial from './components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Initial />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react"
+
+class Initial extends Component{
+    render(){
+        return(
+            <>
+            <h1>Iniciando o Projeto</h1>
+            <p>Montagem de sanduiches</p>
+            </>
+        )
+    }
+}
+
+export default Initial
